67a31623b587c4a72effa77e46252a0671c83351
Mohammedakbarp <90596630+Mohammedakbarp@users.noreply.github.com>
2021-09-15 00:22:28 +0530

    Add files via upload

---
+++ Greatest.java
@@ -0,0 +1,17 @@
+package project;
+
+public class Greatest {
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 8 ;
+		
+		if(x>y)		
+            System.out.println(x);
+		else
+			System.out.println(y);
+			
+			 
+		 
+
+	}
+}
